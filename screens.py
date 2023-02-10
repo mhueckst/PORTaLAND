b1a6278942ffcbca7a992d1695c11975d146a19d
Russ Pedersen <rped2@pdx.edu>
2023-02-09 17:34:15 -0800

    added instructions view

--- screens.py
+++ screens.py
@@ -5,3 +5,40 @@ written by the Firm (trying out creepy anonymous corporation names)
 """
 
 import arcade
+import random
+import os
+
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+
+SCREEN_TITLE = "Instructions"
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SPRITE_SCALING = 0.5
+
+
+class InstructionView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.ORANGE_PEEL)
+
+    def on_draw(self):
+        self.clear()
+        self.texture = arcade.load_texture("./assets/screen/instructions.png")
+
+        arcade.set_viewport(0, SCREEN_WIDTH - 1, 0, SCREEN_HEIGHT - 1)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        self.window.show_view(game_view)
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = InstructionView()
+    window.show_view(start_view)
+#    start_view.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
