753d090d77ad7bd7b94a92257ae0d8540d4d9f84
natasha <nnaylor@pdx.edu>
2023-02-24 12:33:00 -0800

    Refactored gameview.py for readability

--- code/gameview.py
+++ code/gameview.py
@@ -16,6 +16,7 @@ class GameView(arcade.View):
     def __init__(self) -> None:
         super().__init__()
 
+        # Sprite lists
         self.background = None
         self.background2 = None
         self.ground = None
@@ -24,23 +25,31 @@ class GameView(arcade.View):
         self.exit = None
         self.portal_walls = None
 
-        self.player_list = None
+        # Container to hold sprite lists
+        self.sprite_list = None
 
         self.physics_engine = Optional[arcade.PymunkPhysicsEngine]
 
         self.level = 1
 
+        # Variables to track if WASD keys are pressed
         self.A_pressed: bool = False
         self.D_pressed: bool = False
         self.W_pressed: bool = False
         self.S_pressed: bool = False
 
+    def setup(self):
+
         # Set default background color
         background_color = arcade.color.FRESH_AIR
         arcade.set_background_color(background_color)
 
-    def setup(self):
+        self.map_setup()
+        self.sprite_setup()
+        self.physics_engine_setup()
+        self.add_sprites_to_physics_engine()
 
+    def map_setup(self):
         # Map name
         map_name = f"level_{self.level:02}.tmx"
         map_path = ASSETS_PATH / "maps" / map_name
@@ -59,10 +68,13 @@ class GameView(arcade.View):
         self.exit = tile_map.sprite_lists["exit"]
         self.portal_walls = tile_map.sprite_lists["portal walls"]
 
-        # Create sprite list
-        self.player_list = arcade.SpriteList()
+    def sprite_setup(self):
+        # Initialize sprite list
+        self.sprite_list = arcade.SpriteList()
 
         # Create player sprite
+        # NOTE: Another parameter could be added to this class to
+        #       handle portal_wall collisions, see player.py
         self.player_sprite = player.Player(self.ladders)
 
         # Set player starting location
@@ -70,8 +82,9 @@ class GameView(arcade.View):
         self.player_sprite.center_y = vc.STARTING_Y
 
         # Add to player sprite list
-        self.player_list.append(self.player_sprite)
+        self.sprite_list.append(self.player_sprite)
 
+    def physics_engine_setup(self):
         # Set damping and gravity
         damping = pc.DEF_DAMPING
         gravity = (0, -(pc.GRAVITY))
@@ -80,6 +93,7 @@ class GameView(arcade.View):
         self.physics_engine = arcade.PymunkPhysicsEngine(
             damping=damping, gravity=gravity)
 
+    def add_sprites_to_physics_engine(self):
         # Add player to physics engine
         self.physics_engine.add_sprite(self.player_sprite,
                                        friction=pc.PLAYER_FRICTION,
@@ -111,12 +125,14 @@ class GameView(arcade.View):
             self.D_pressed = True
         elif key == arcade.key.W:
             self.W_pressed = True
+            # Player jumps on W press iff on ground and not on ladder
             if self.physics_engine.is_on_ground(self.player_sprite) \
                     and not self.player_sprite.is_on_ladder:
                 strength = (0, pc.PLAYER_JUMP_STRENGTH)
                 self.physics_engine.apply_impulse(self.player_sprite, strength)
         elif key == arcade.key.S:
             self.S_pressed = True
+            # Player goes down ladder on S press iff on ladder
             if self.player_sprite.is_on_ladder:
                 self.player_sprite.change_y = -(pc.PLAYER_MAX_SPEED_VERT)
                 self.physics_engine.set_velocity(
@@ -130,6 +146,7 @@ class GameView(arcade.View):
         elif key == arcade.key.W:
             self.W_pressed = False
         elif key == arcade.key.S:
+            # Player stays on ladder when S is released
             if self.player_sprite.is_on_ladder:
                 self.player_sprite.change_y = 0
             self.S_pressed = False
@@ -137,40 +154,61 @@ class GameView(arcade.View):
     def on_update(self, delta: float):
         is_on_ground = self.physics_engine.is_on_ground(self.player_sprite)
 
-        # Update player force based on keys pressed
+        # Update player movement based on keys pressed
         if self.A_pressed and not self.D_pressed:
-            if is_on_ground or self.player_sprite.is_on_ladder:
-                force = (-(pc.PLAYER_MOVE_FORCE_ON_GROUND), 0)
-            else:
-                force = (-(pc.PLAYER_MOVE_FORCE_IN_AIR), 0)
-            self.physics_engine.apply_force(self.player_sprite, force)
-            self.physics_engine.set_friction(self.player_sprite, 0)
+            self.apply_player_movement('A', is_on_ground, 0)
         elif self.D_pressed and not self.A_pressed:
-            if is_on_ground or self.player_sprite.is_on_ladder:
-                force = (pc.PLAYER_MOVE_FORCE_ON_GROUND, 0)
-            else:
-                force = (pc.PLAYER_MOVE_FORCE_IN_AIR, 0)
-            self.physics_engine.apply_force(self.player_sprite, force)
-            self.physics_engine.set_friction(self.player_sprite, 0)
+            self.apply_player_movement('D', is_on_ground, 0)
         elif self.W_pressed and not self.S_pressed:
             if self.player_sprite.is_on_ladder:
-                force = (0, pc.PLAYER_MOVE_FORCE_ON_GROUND)
-                self.physics_engine.apply_force(self.player_sprite, force)
-                self.physics_engine.set_friction(self.player_sprite, 0)
+                self.apply_player_movement('W', is_on_ground, 0)
         elif self.S_pressed and not self.W_pressed:
             if self.player_sprite.is_on_ladder:
-                force = (0, -(pc.PLAYER_MOVE_FORCE_ON_GROUND))
-                self.physics_engine.set_friction(self.player_sprite, 0)
+                self.apply_player_movement('S', is_on_ground, 0)
 
         else:
-            self.physics_engine.set_friction(self.player_sprite, 1.0)
+            self.set_player_friction(1.0)
 
         # Update movement in physics engine
         self.physics_engine.step()
+
+        # Update sprites to stay in screen bounds
         self.keep_sprites_within_bounds()
 
+    def apply_player_movement(self, key, is_on_ground, friction):
+        force = self.get_force(key, is_on_ground)
+        self.apply_player_force(force)
+        self.set_player_friction(friction)
+
+    def get_force(self, key, is_on_ground) -> int:
+        match key:
+            case 'A':
+                if is_on_ground or self.player_sprite.is_on_ladder:
+                    return (-(pc.PLAYER_MOVE_FORCE_ON_GROUND), 0)
+                else:
+                    return (-(pc.PLAYER_MOVE_FORCE_IN_AIR), 0)
+            case 'D':
+                if is_on_ground or self.player_sprite.is_on_ladder:
+                    return (pc.PLAYER_MOVE_FORCE_ON_GROUND, 0)
+                else:
+                    return (pc.PLAYER_MOVE_FORCE_IN_AIR, 0)
+            case 'W':
+                if self.player_sprite.is_on_ladder:
+                    return (0, pc.PLAYER_MOVE_FORCE_ON_GROUND)
+            case 'S':
+                if self.player_sprite.is_on_ladder:
+                    return (0, -(pc.PLAYER_MOVE_FORCE_ON_GROUND))
+            case _:
+                return
+
+    def apply_player_force(self, force):
+        self.physics_engine.apply_force(self.player_sprite, force)
+
+    def set_player_friction(self, friction):
+        self.physics_engine.set_friction(self.player_sprite, friction)
+
     def keep_sprites_within_bounds(self):
-        for sprite in self.player_list:
+        for sprite in self.sprite_list:
             if sprite.right > vc.SCREEN_WIDTH:
                 sprite.right = vc.SCREEN_WIDTH - 24
             elif sprite.left < 0:
@@ -189,4 +227,4 @@ class GameView(arcade.View):
         self.portal_walls.draw()
         self.ladders.draw()
         self.exit.draw()
-        self.player_list.draw()
+        self.sprite_list.draw()
