af100c9d1c9b9bb5a7c60e41cb50216a2ee3f88e
Max Huecksteadt <mhueck2@pdx.edu>
2023-02-26 11:43:29 -0800

    Added spritesheets and portal class

---
+++ code/portal.py
@@ -0,0 +1,23 @@
+"""
+Portal class, initializes portal animation
+"""
+import arcade
+
+
+class Portal(arcade.Sprite):
+    """ This class creates a portal animation """
+
+    def __init__(self, texture_list):
+        super().__init__()
+
+        # Start at the first frame
+        self.current_texture = 0
+        self.textures = texture_list
+
+    def update(self):
+
+        self.current_texture += 1
+        if self.current_texture < len(self.textures):
+            self.set_texture(self.current_texture)
+        else:
+            self.remove_from_sprite_lists()
